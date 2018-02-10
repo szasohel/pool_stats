e2536c09477a990c1d46b5da3467cc1dbb4cfc17
Sayed Sohel <szasohel@gmail.com>
2018-02-10 05:59:10 -0500

    feat: route and templates added

---
+++ views.py
@@ -0,0 +1,51 @@
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from model import Base, Stats
+from flask import Flask, render_template, url_for
+from flask import request, redirect
+
+
+app = Flask(__name__)
+
+# connect to database
+engine = create_engine('sqlite:///Stats.db')
+Base.metadata.bind = engine
+
+# create database session
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+
+@app.route('/playerlist')
+def player_list():
+	
+
+@app.route('/createplayer', methods=['GET', 'POST'])
+def create_player():
+	
+
+
+@app.route('/startgame', methods=['GET', 'POST'])
+def start_game():
+	
+
+
+@app.route('/playgame/<string:player1>vs<string:player2>/', methods=['GET', 'POST'])
+def play_game(player1,player2):
+
+
+
+@app.route('/leaderboard')
+def leaderboard():
+	
+
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()

ab5c4f5bc544b21977640ffef1bc97d9d5119e77
Sayed Sohel <szasohel@gmail.com>
2018-02-10 06:04:00 -0500

    feat: player list page created

--- views.py
+++ views.py
@@ -23,22 +23,21 @@ def home():
 
 @app.route('/playerlist')
 def player_list():
-	
+	players = session.query(Stats).order_by(asc(Stats.player))
+	return render_template('player_list.html', players=players)
+
 
 @app.route('/createplayer', methods=['GET', 'POST'])
 def create_player():
 	
 
-
 @app.route('/startgame', methods=['GET', 'POST'])
 def start_game():
 	
 
-
 @app.route('/playgame/<string:player1>vs<string:player2>/', methods=['GET', 'POST'])
 def play_game(player1,player2):
-
-
+	
 
 @app.route('/leaderboard')
 def leaderboard():
